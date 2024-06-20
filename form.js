1d08944f4451f18b14dbf20bf51f27683531fb33
Diverse2 <ntwalie83@gamil.com>
2024-06-20 11:17:20 +0200

    updates

--- form.js
+++ form.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     inputControl.classList.add("success");
     inputControl.classList.remove("error");
   };
-
+// Email Validation
   const isValidEmail = (email) => {
     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return pattern.test(String(email).toLowerCase());
